c987bb367f376af5428b3e45515546337668502f
anhuynh1998 <huynhxuanan199@gmail.com>
2022-09-09 15:38:40 +0700

    fistcommit

---
+++ noti.js
@@ -0,0 +1,21 @@
+
+
+const toast = document.getElementById( "toast_1" );
+const toastShow = toast.querySelectorAll( ".toast_show" );
+const positionToast = [ 'toast--top--right', 'toast--top--left', 'toast--bottom--left', 'toast--bottom--right' ];
+
+
+const random = () => {
+    const randomPosition = Math.floor( Math.random() * positionToast.length );
+    const randomToast = Math.floor( Math.random() * toastShow.length );
+    let x = positionToast[ randomPosition ];
+    let y = toastShow[ randomToast ]
+    y.classList.toggle( x )
+    setTimeout( function () {
+        y.classList.toggle( x )
+        
+    },3300)
+    
+}
+
+
